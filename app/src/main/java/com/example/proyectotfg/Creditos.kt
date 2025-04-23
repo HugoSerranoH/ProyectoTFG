aeebe822f98dd033adb25620f003fbae41d223c6
HugoSerranoH <hugoserranohz@gmail.com>
2025-04-23 14:30:27 +0200

    Mejoras en la interfaz,añadido creditos y añadido boton para volver a elegir deporte.

--- app/src/main/java/com/example/proyectotfg/Creditos.kt
+++ app/src/main/java/com/example/proyectotfg/Creditos.kt
@@ -1,12 +1,18 @@
 package com.example.proyectotfg
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Creditos : AppCompatActivity() {
+    private lateinit var botongithub : ImageView
+    private lateinit var botonlinkedin : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,20 @@ class Creditos : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        botongithub = findViewById(R.id.imageViewgithubicon)
+        botonlinkedin = findViewById(R.id.imageViewlinkedinicon)
+
+        botongithub.setOnClickListener {
+            val githubUrl = "https://github.com/HugoSerranoH/ProyectoTFG"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl))
+            startActivity(intent)
+        }
+
+        botonlinkedin.setOnClickListener {
+            val linkedinUrl = "https://www.linkedin.com/in/hugo-serrano-hernández-baaa512a6"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedinUrl))
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/proyectotfg/SeleccionarCarrera.kt
+++ app/src/main/java/com/example/proyectotfg/SeleccionarCarrera.kt
@@ -1,18 +1,25 @@
 package com.example.proyectotfg
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SeleccionarCarrera : AppCompatActivity() {
+    private lateinit var botonElegirModo :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seleccionar_carrera)
-
+        botonElegirModo = findViewById(R.id.buttonElegirDeporte)
         val idDeporte = intent.getIntExtra("id_deporte", -1)
         Log.i("DEBUG", "ID del deporte recibido en Seleccionar_Carrera: $idDeporte")
 
+        botonElegirModo.setOnClickListener {
+            val intent = Intent(this, EligeModo::class.java)
+            startActivity(intent)
 
+        }
         val fragment = ListaCarreras().apply {
             arguments = Bundle().apply {
                 putInt("id_deporte", idDeporte)
